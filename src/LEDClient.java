f1091229bce432991a5aa10115d69c8587c57340
netlab-student <pconstance106@mycr.redwoods.edu>
2022-10-19 12:40:25 -0700

    Square Gradient

--- src/LEDClient.java
+++ src/LEDClient.java
@@ -28,14 +28,52 @@ public class LEDClient {
         System.out.println(message);
         zsocket.send(message);
     }
-
-    public void blinkN(int[] color, int times, int miliseconds) throws  InterruptedException{
-        for(int i=0; i<times; i++) {
-            send(color);
-            TimeUnit.MILLISECONDS.sleep(miliseconds);
-            send(LEDClient.OFF);
-            TimeUnit.MILLISECONDS.sleep(miliseconds);
+    public void SquareGrade(int spd) throws InterruptedException {
+        send(LEDClient.OFF);
+        int red=0;
+        int green=0;
+        int blue=0;
+        for (int i=0;i<255;i+=spd) {
+            red = i;
+            green = 0;
+            blue = 0;
+            int[] color1 = {red,green,blue};
+            send(color1);
+            TimeUnit.MILLISECONDS.sleep(100);
         }
+        for (int i=255;i>0;i+=spd) {
+            red=0-i;
+            green=i;
+            blue=85;
+            int[] color2 = {red,green,blue};
+            send(color2);
+            TimeUnit.MILLISECONDS.sleep(100);
+        }
+        for (int i=0;i<255;i+=spd) {
+            red=0;
+            green=255-i;
+            blue=i;
+            int[] color3 = {red,green,blue};
+            send(color3);
+            TimeUnit.MILLISECONDS.sleep(100);
+        }
+        for (int i=255;i<157;i+=spd) {
+            red=i;
+            green=0;
+            blue=255-i;
+            int[] color4 = {red,green,blue};
+            send(color4);
+            TimeUnit.MILLISECONDS.sleep(100);
+        }
+        for (int i=0;i<162;i+=spd) {
+            red=255-i;
+            green=0;
+            blue=58-i;
+            int[] color5 = {red,green,blue};
+            send(color5);
+            TimeUnit.MILLISECONDS.sleep(100);
+        }
+        send(LEDClient.OFF);
     }
 
     public void close() throws InterruptedException {
@@ -48,7 +86,7 @@ public class LEDClient {
         LEDClient ledClient = new LEDClient("tcp", "192.168.86.250", 5001);
         try {
             int[] color = {0, 0, 255};
-            ledClient.blinkN(color, 5, 1000);
+            ledClient.SquareGrade(10);
             ledClient.close();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
